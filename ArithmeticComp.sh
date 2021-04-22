927bf0f6801bf3e67110825f59444d9f52f32955
karuna <karunakakade8@gmail.com>
2021-04-23 00:52:57 +0530

    Added: take inpute and perform computation

--- ArithmeticComp.sh
+++ ArithmeticComp.sh
@@ -3,10 +3,5 @@
 declare -A AllResults
 echo "take inputs for a,b,c: "
 read a b c
-
-AllResults[res1]=$((a+b*c))
-AllResults[res2]=$((a*b+c))
-AllResults[res3]=$((c+a/b))
-AllResults[res4]=$((a%b+c))
-
-echo "All Results from dictionary: " ${AllResults[@]}
+result=$((a*b+c))
+echo $result
