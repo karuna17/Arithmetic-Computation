eab722ac53b129522e008ed279792743ac2fb2ee
karuna <karunakakade8@gmail.com>
2021-04-23 20:44:09 +0530

    Added: read values from dictionary to array

--- ArithmeticComp.sh
+++ ArithmeticComp.sh
@@ -1,7 +1,22 @@
 #!/bin/bash -x
 
 declare -A AllResults
+declare -a Array
+
 echo "take inputs for a,b,c: "
 read a b c
-result=$((a*b+c))
-echo $result
+
+res1=$((a+b*c))
+res2=$((a*b+c))
+res3=$((c+a/b))
+res4=$((a%b+c))
+AllResults[op1]=$res1
+AllResults[op2]=$res2
+AllResults[op3]=$res3
+AllResults[op4]=$res4
+
+echo "Dictionary: " ${AllResults[@]}
+
+Array[0]=${AllResults[@]}
+
+echo "Array: " ${Array[@]}
